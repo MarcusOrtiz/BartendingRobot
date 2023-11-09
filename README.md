b05af956835da42270271fa3d9dc551bac7d56eb
Marcus Ortiz <mjo24@duke.edu>
2023-11-09 18:13:13 -0500

    Visual gripper working state, with inproper scaling

---
+++ README.md
@@ -0,0 +1,7 @@
+To preview the robot visually in rviz, you can make navigate to the catkin environment and
+run the following commands
+```
+catkin_make
+source devel/setup.bash
+roslaunch bartending_robot display.launch
+```
---
+++ catkin_ws/build/bartending_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mjo24/BartendingRobot/catkin_ws/src/bartending_robot
+# Build directory: /home/mjo24/BartendingRobot/catkin_ws/build/bartending_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
