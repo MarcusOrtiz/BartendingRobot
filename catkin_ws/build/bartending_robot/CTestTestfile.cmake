f6472d97a23331a98172cb37d9d5856ca1a000ae
Jessica Tiu <jgt16@duke.edu>
2023-11-10 13:00:34 -0500

    Remove build files to get rid of catkin_make error

--- catkin_ws/build/bartending_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mjo24/BartendingRobot/catkin_ws/src/bartending_robot
-# Build directory: /home/mjo24/BartendingRobot/catkin_ws/build/bartending_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
