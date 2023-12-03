0777d6596e0645c4ede035f3c1924b72e619cce0
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:44 -0500

    Initialize UR5eMoveGroupPythonInterface

--- catkin_ws/src/bartending_robot/moveit/motion_planning.py
+++ catkin_ws/src/bartending_robot/moveit/motion_planning.py
@@ -1,13 +1,62 @@
+import sys
 import rospy
+import moveit_commander
+import moveit_msgs.msg
+
+
+class UR5eMoveGroupPythonInterface(object):
+    def __init__(self):
+        super(UR5eMoveGroupPythonInterface, self).__init__()
+
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("ur5e_move_group_python_interface", anonymous=True)
+
+        ## Instantiate a `RobotCommander`_ object. Provides information such as the robot's
+        ## kinematic model and the robot's current joint states
+        robot = moveit_commander.RobotCommander()
+
+        ## Instantiate a `PlanningSceneInterface`_ object.  This provides a remote interface
+        ## for getting, setting, and updating the robot's internal understanding of the
+        ## surrounding world:
+        scene = moveit_commander.PlanningSceneInterface()
+
+        ## Instantiate a `MoveGroupCommander`_ object.  This object is an interface
+        ## to a planning group (group of joints).
+        ## This interface can be used to plan and execute motions:
+        group_name = "manipulator"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+
+        ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+        ## trajectories in Rviz:
+        display_trajectory_publisher = rospy.Publisher(
+            "/move_group/display_planned_path",
+            moveit_msgs.msg.DisplayTrajectory,
+            queue_size=20,
+        )
+
+        # Name of the end-effector link for this group:
+        eef_link = move_group.get_end_effector_link()
+        print("============ End effector link: %s" % eef_link)
+
+        print("============ Printing robot state")
+        print(robot.get_current_state())
+
+        # Misc variables
+        self.robot = robot
+        self.scene = scene
+        self.move_group = move_group
+        self.display_trajectory_publisher = display_trajectory_publisher
+        self.eef_link = eef_link
 
 
 def main():
     try:
-        return
+        umg = UR5eMoveGroupPythonInterface()
     except rospy.ROSInterruptException:
         return
     except KeyboardInterrupt:
         return
+
 
 if __name__ == "__main__":
     main()
