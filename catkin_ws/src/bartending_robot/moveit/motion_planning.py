c11cd02928778e26661be4404430febe7b99df84
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:44 -0500

    Initialize main function

--- catkin_ws/src/bartending_robot/moveit/motion_planning.py
+++ catkin_ws/src/bartending_robot/moveit/motion_planning.py
@@ -1,5 +1,13 @@
+import rospy
+
+
 def main():
-    pass
+    try:
+        return
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
 
 if __name__ == "__main__":
     main()
