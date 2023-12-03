848b3d3d5f07260a1ed2b5c2418ef127e7c342a3
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:44 -0500

    Remove catkin setup from setup script

--- catkin_ws/src/bartending_robot/moveit/setup.py
+++ catkin_ws/src/bartending_robot/moveit/setup.py
@@ -6,24 +6,6 @@ from pathlib import Path
 class Scene(object):
     def __init__(self):
         pass
-    
-    def setup_catkin(self):
-        curr_dir = Path.cwd().absolute()  # looks redundant, but I'm just initializing
-
-        if "catkin_ws" in curr_dir.parts:
-            while "catkin_ws" in curr_dir.parts and "catkin_ws" != curr_dir.stem:
-                print(f"Have not arrived at `catkin_ws` yet. Currently at {Path.cwd()}")
-                os.chdir(Path.cwd().parent)
-                curr_dir = Path.cwd().absolute()
-            print(f"Arrived at {curr_dir}")
-        else:
-            print("Error: `catkin_ws` not found.")
-
-        os.system("catkin_make &")
-        time.sleep(5)
-        os.system("source devel/setup.bash &")
-        time.sleep(5)
-
 
     def launchGazebo(self):
         os.system("roslaunch ur5e_gripper_gazebo ur5e_gripper_bringup.launch &")
@@ -36,7 +18,6 @@ class Scene(object):
         time.sleep(5)
 
     def sceneSetup(self):
-        self.setup_catkin()
         self.launchGazebo()
         self.launchMoveIt()
 
