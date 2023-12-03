b3e495abd278e3708d427a58e11679474ecc5a6f
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:43 -0500

    Add launch script

---
+++ catkin_ws/src/bartending_robot/moveit/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import os
+import time
+
+
+class Scene(object):
+    def __init__(self):
+        pass
+
+    def launchGazebo(self):
+        os.system("roslaunch ur_gazebo ur5e_bringup.launch &")
+        time.sleep(5)
+
+    def launchMoveIt(self):
+        os.system(
+            "roslaunch ur5_moveit_config moveit_planning_execution.launch sim:=true"
+        )
+        time.sleep(5)
+
+    def sceneSetup(self):
+        self.launchGazebo()
+        self.launchMoveIt()
+
+
+if __name__ == "__main__":
+    scene = Scene()
+    scene.sceneSetup()
+
