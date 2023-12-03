2352e6d0377262638742339ff0d6aeb7ff91011b
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:44 -0500

    Fix setup script

--- catkin_ws/setup.py
+++ catkin_ws/setup.py
@@ -1,32 +1,23 @@
 #!/usr/bin/python3
 import os
-import time
 
 
 class Scene(object):
     def __init__(self):
         pass
 
-    def source_workspace(self):
-        os.system("bash -c \"source $HOME/BartendingRobot/catkin_ws/devel/setup.bash\"")
-
-    def launchGazebo(self):
-        os.system("roslaunch ur5e_gripper_gazebo ur5e_gripper_bringup.launch &")
-        time.sleep(5)
-
-    def launchMoveIt(self):
-        os.system(
-            "roslaunch ur5_moveit_config moveit_planning_execution.launch sim:=true"
-        )
-        time.sleep(5)
-
     def sceneSetup(self):
-        self.source_workspace()
-        self.launchGazebo()
-        self.launchMoveIt()
+        commands = [
+            "roslaunch ur5e_gripper_gazebo ur5e_gripper_bringup.launch",
+            "sleep 10 ; roslaunch ur5_moveit_config moveit_planning_execution.launch sim:=true",
+        ]
+
+        for command in commands:
+            os.system(
+                f'bash -c "source $HOME/BartendingRobot/catkin_ws/devel/setup.bash ; {command} &"'
+            )
 
 
 if __name__ == "__main__":
     scene = Scene()
     scene.sceneSetup()
-
