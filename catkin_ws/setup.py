5489037c9d281a78be05ef17e678cb019451ec3e
jgtiu <tjessica10@gmail.com>
2023-12-03 01:35:44 -0500

    Remove change dir lines

--- catkin_ws/setup.py
+++ catkin_ws/setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 import time
-from pathlib import Path
+
 
 class Scene(object):
     def __init__(self):
@@ -9,7 +9,6 @@ class Scene(object):
 
     def source_workspace(self):
         os.system("bash -c \"source $HOME/BartendingRobot/catkin_ws/devel/setup.bash\"")
-        os.chdir(f"{os.path.expanduser('~')}/BartendingRobot/catkin_ws")
 
     def launchGazebo(self):
         os.system("roslaunch ur5e_gripper_gazebo ur5e_gripper_bringup.launch &")
